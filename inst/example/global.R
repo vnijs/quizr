d049f4aefe74c73670c70dd1e21a08095fb78e30
mostly-harmless <vnijs@ucsd.edu>
2015-06-17 06:57:24 -0700

    Evaluate the Rmd file in the shiny-environment

--- inst/example/global.R
+++ inst/example/global.R
@@ -1,5 +1,5 @@
 ## loading / installing dependencies as needed
-dep <- c("shiny", "ggplot2", "rmarkdown", "dplyr", "knitr", "tidyr", "grid", "shinythemes")
+dep <- c("shiny", "ggplot2", "rmarkdown", "dplyr", "knitr", "tidyr", "grid", "shinythemes", "pryr")
 
 for (d in dep) {
   if (!require(d, character.only = TRUE)) {
@@ -31,9 +31,6 @@ addResourcePath("js", "www/js/")
 knitr::opts_chunk$set(echo = FALSE, comment = NA, cache = FALSE,
                       message = FALSE, warning = FALSE)
 
-## functions for rendering (r)markdown and modals
-source("./bootcamp.R")
-
 ## environment to hold session information
 r_sessions <- new.env(parent = emptyenv())
 
@@ -42,3 +39,29 @@ r_sessions <- new.env(parent = emptyenv())
 
 ## case names
 cases <- list("1" = "1. Demand and Cost")
+
+## create a bootstrap modal
+bs_modal <- function(modal_title, link, file) {
+  sprintf("<div class='modal fade' id='%s' tabindex='-1' role='dialog' aria-labelledby='%s_label' aria-hidden='true'> <div class='modal-dialog'> <div class='modal-content'> <div class='modal-header'> <button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button> <h4 class='modal-title' id='%s_label'>%s</h4> </div> <div class='modal-body'> %s </div> </div> </div> </div> <button type='button' data-toggle='modal' data-target='#%s' class='btn btn-success' aria-label='Left Align'> <span class='glyphicon glyphicon-film' aria-hidden='true'></span> Video </button>",
+    link, link, link, modal_title, file, link) %>% HTML
+}
+
+## function to render .md files to html
+inclMD <- function(path) {
+  markdown::markdownToHTML(path, fragment.only = TRUE, options = c(""),
+                           stylesheet="../www/empty.css")
+}
+
+## function to render .Rmd files to html - does not embed image or add css
+inclRmd <- function(path, r_env) {
+  paste(readLines(path, warn = FALSE), collapse = '\n') %>%
+  knitr::knit2html(text = ., fragment.only = TRUE, quiet = TRUE, envir = r_env,
+                   options = "", stylesheet = "../www/empty.css") %>%
+    gsub("&lt;!--/html_preserve--&gt;","",.) %>%  ## knitr adds this
+    gsub("&lt;!--html_preserve--&gt;","",.) %>%   ## knitr adds this
+    HTML %>%
+    withMathJax
+}
+
+## check if a button WAS pressed
+is_empty <- function(x, empty = "") if (is.null(x) || x == empty) TRUE else FALSE
--- inst/example/server.R
+++ inst/example/server.R
@@ -2,6 +2,12 @@ shinyServer(function(input, output, session) {
 
   ## set the table for the quiz questions
   source("./init.R", local = TRUE)
+
+  ## locating the environment used by shiny
+  ## r_quiz is a reactiveValues object defined in init.R
+  r_env <- pryr::where("r_quiz")
+
+  ## set the table for the quiz questions
   source("./quiz.R", local = TRUE)
 
   ## get content for each of the mini cases
